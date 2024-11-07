138d1779a0ceb6f00da24ab5969a8a58da616b61
bobr-ik <aavramenko804@gmail.com>
2024-11-07 11:30:25 +0300

    commit

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from data.orm import SyncORM
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,7 +12,8 @@ SyncORM.insert_data()
 @app.route('/', methods=['GET'])
 def hello_world():
     data = request.args.get('id')
-    return SyncORM.select_data(data)
+    data = 1
+    return jsonify({'site': render_template('temp1.html', data=SyncORM.select_data(data)), 'data': data})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
