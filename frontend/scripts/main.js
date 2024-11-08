b233e2f3a73b750fcb0a0e05f02d939c08e8f218
bobr-ik <aavramenko804@gmail.com>
2024-11-08 23:33:40 +0300

    Update main.js

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,5 +1,5 @@
 async function req(){
-    a = await fetch('localhost:5000/api?id=1')
+    a = await fetch('http://158.160.85.97:5000/api?id=1')
     a = await a.json()
     console.log(a)
     document.getElementById('res').innerHTML = a['site']
